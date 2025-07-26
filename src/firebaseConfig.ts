77b0cc9407343d91d1fd785058fa83e5e3dbb163
DEVISETTI SAI PAVAN KUMAR <devisettisaipavankumar@gmail.com>
2025-07-26 18:23:50 +0530

    Updated firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,15 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBu4UA4IjuVxC_bnr7Yn55qyB5P8ZWJhZk",
+  authDomain: "med-2870d.firebaseapp.com",
+  projectId: "med-2870d",
+  storageBucket: "med-2870d.firebasestorage.app",
+  messagingSenderId: "11019310072",
+  appId: "1:11019310072:web:f202d0ff68212b6cb1fc0d",
+  measurementId: "G-F5PFT711P2"
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
